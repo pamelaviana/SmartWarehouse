d358b1ddb1894cd5bf172c7627931d018895c906
pamelaviana <118546667+pamelaviana@users.noreply.github.com>
2023-04-13 00:28:37 +0100

    StockRoom registration
    Created registration DNS

---
+++ src/main/java/com/proto/StockRoomService/StockRoomRegistration.java
@@ -0,0 +1,113 @@
+package com.proto.StockRoomService;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.util.Properties;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+
+
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+public class StockRoomRegistration {
+
+	static int port = 50052;
+
+
+	public static void main ( String[] args) throws InterruptedException, IOException {
+				
+		Properties prop = StockRoomRegistration.getProperties();
+		StockRoomRegistration.registerService(prop);
+
+
+		int port = Integer.valueOf( prop.getProperty("service_port") );// #.9099;
+
+	
+		
+		try {
+			Server server = ServerBuilder
+				.forPort(port).addService(new StockRoomServer())
+				.build();
+		server.start();
+		
+		System.out.println(" Server 1 started at  "  + port);
+	
+
+		server.awaitTermination();
+
+		} catch (IOException | InterruptedException e) {
+			System.err.println("Error starting server");
+			e.printStackTrace();
+
+		}
+			
+	}	
+	
+	
+	private static Properties getProperties() {
+		
+		Properties prop = null;		
+
+		 try (InputStream input = new FileInputStream("src/main/resources/StockRoomService.properties")) {
+
+	            prop = new Properties();
+
+	         // load a properties file
+	            prop.load(input);
+
+	            // get the property value and print it out
+	            System.out.println("StockRoom Service properies ...");
+	            System.out.println("\t service_type: " + prop.getProperty("service_type"));
+	            System.out.println("\t service_name: " +prop.getProperty("service_name"));
+	            System.out.println("\t service_description: " +prop.getProperty("service_description"));
+		        System.out.println("\t service_port: " +prop.getProperty("service_port"));
+		 } catch (IOException ex) {
+	            ex.printStackTrace();
+	        }
+	
+		 return prop;
+	}
+	
+	
+	private static  void registerService(Properties prop) {
+		
+		
+		 try {
+	            // Create a JmDNS instance
+	            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+	            String service_type = prop.getProperty("service_type") ;//"_stockRoomService._tcp.local.";
+	            String service_name = prop.getProperty("service_name")  ;// "example";
+	            int service_port = Integer.valueOf( prop.getProperty("service_port") );// #.50052;
+
+		
+	            String service_description_properties = prop.getProperty("service_description")  ;//"path=index.html";
+	            // Register a service
+	            ServiceInfo serviceInfo = ServiceInfo.create(service_type, service_name, service_port, service_description_properties);
+	            jmdns.registerService(serviceInfo);
+	            
+	            System.out.printf("registrering service with type %s and name %s \n", service_type, service_name);
+	            
+	            // Wait a bit
+	            Thread.sleep(1000);
+	            
+	         // Unregister all services
+	           jmdns.unregisterAllServices();
+	           
+	           
+		  } catch (IOException e) {
+	            System.out.println(e.getMessage());
+	        } catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}
+
+
+
+}	
